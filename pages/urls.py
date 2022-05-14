b0bf758f50e805e64aebcb8aabfc36426adb913e
medofayed <mfedo32@gmail.com>
2022-05-14 21:24:17 +0200

    added testing

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,16 @@
 # pages/urls.py
-from django.urls import path
+# from django.urls import path
 
-from .views import homePageView
+# from .views import homePageView
+
+# urlpatterns = [
+#     path("", homePageView, name="home"),
+# ]
+
+from django.urls import path
+from .views import HomePageView, AboutPageView  # new
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),  # new
+    path("", HomePageView.as_view(), name="home"),
 ]
